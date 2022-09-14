82a4a12d2d9c85f413a2e355e70a69c227f99ceb
URK21CS2053 <109648256+URK21CS2053@users.noreply.github.com>
2022-09-14 09:40:08 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -50,44 +50,5 @@ for i in range(len(operations)):
     queue.enqueue(int(data[i]))
   elif operations[i] == "dequeue":
     queue.dequeue()
-queue.status()class Node:
-  def __init__(self, data):
-    self.data = data
-    self.next = None
-
-
-class Queue:
-  def __init__(self):
-    self.head = None
-    self.last = None
-
-  def enqueue(self, data) -> None:
-    # Write your code here
-    if self.last is None:
-      self.head = Node(data)
-      self.last = self.head
-    else:
-      self.last.next = Node(data)
-      self.last = self.last.next
-
-  def dequeue(self) -> None:
-    # Write your code here
-    if self.head is None:
-      return None
-    else:
-      val_returned = self.head.data
-      self.head = self.head.next
-
-  def status(self) -> None:
-    # Write your code here
-    current = self.head
-    status_of_queue = []
-    while (current):
-      status_of_queue.append(current.data)
-      current = current.next
-    for element in status_of_queue:
-      print(element, end="=>")
-    print(None)
-
-
+queue.status()
 
